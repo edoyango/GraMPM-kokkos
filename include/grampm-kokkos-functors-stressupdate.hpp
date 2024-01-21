0b13d574badfd33c230ff029d4aa86098b4324b5
Edward <edward_yang_125@hotmail.com>
2024-01-21 14:36:22 +1100

    add dp model

--- include/grampm-kokkos-functors-stressupdate.hpp
+++ include/grampm-kokkos-functors-stressupdate.hpp
@@ -10,18 +10,27 @@ namespace GraMPM {
 
                 template<typename F>
                 struct hookes_law {
-                    double E, v, dt;
+
+                    double E, v, D0, dt;
                     const Kokkos::View<F*[6]> p_sigma, p_strainrate;
                     const Kokkos::View<F*[3]> p_spinrate;
+
                     hookes_law(Kokkos::View<F*[6]> p_sigma_, Kokkos::View<F*[6]> p_strainrate_, 
                         Kokkos::View<F*[3]> p_spinrate_)
                         : p_sigma {p_sigma_}
                         , p_strainrate {p_strainrate_}
                         , p_spinrate {p_spinrate_}
                     {}
+
+                    void set_E_v(F E_, F v_) {
+                        E = E_;
+                        v = v_;
+                        D0 = E/((1.+v)*(1.-2.*v));
+                    }
+
                     KOKKOS_INLINE_FUNCTION
                     void operator()(const int i) const {
-                        const F D0 {E/((1.+v)*(1.-2.*v))};
+
                         // elastic stiffness matrix
                         F dsigmaxx = D0*((1.-v)*p_strainrate(i, 0) + v*p_strainrate(i, 1) + v*p_strainrate(i, 2));
                         F dsigmayy = D0*(v*p_strainrate(i, 0) + (1.-v)*p_strainrate(i, 1) + v*p_strainrate(i, 2));
@@ -48,6 +57,139 @@ namespace GraMPM {
                         p_sigma(i, 3) += dt*dsigmaxy;
                         p_sigma(i, 4) += dt*dsigmaxz;
                         p_sigma(i, 5) += dt*dsigmayz;
+                    }
+                };
+
+                template<typename F>
+                struct drucker_prager_elastoplastic {
+                    double dt, phi, psi, cohesion, alpha_phi, alpha_psi, k_c, E, v, D0;
+                    const Kokkos::View<F*[6]> p_sigma, p_strainrate;
+                    const Kokkos::View<F*[3]> p_spinrate;
+
+                    drucker_prager_elastoplastic(Kokkos::View<F*[6]> p_sigma_, Kokkos::View<F*[6]> p_strainrate_, 
+                        Kokkos::View<F*[3]> p_spinrate_)
+                        : p_sigma {p_sigma_}
+                        , p_strainrate {p_strainrate_}
+                        , p_spinrate {p_spinrate_}
+                    {}
+
+                    void set_DP_params(F phi_, F psi_, F c_, F E_, F v_) {
+                        phi = phi_;
+                        psi = psi_;
+                        cohesion = c_;
+                        alpha_phi = 2.*std::sin(phi)/(std::sqrt(3.)*(3.-std::sin(phi)));
+                        alpha_psi = 2.*std::sin(psi)/(std::sqrt(3.)*(3.-std::sin(phi)));
+                        k_c = 6.*cohesion*std::cos(phi)/(std::sqrt(3.)*(3.-std::sin(phi)));
+                        E = E_;
+                        v = v_;
+                        D0 = E/((1.+v)*(1.-2.*v));
+                    }
+
+                    KOKKOS_INLINE_FUNCTION
+                    void operator()(const int i) const {
+
+                        // elastic stiffness matrix
+                        F dsigmaxx = D0*((1.-v)*p_strainrate(i, 0) + v*p_strainrate(i, 1) + v*p_strainrate(i, 2));
+                        F dsigmayy = D0*(v*p_strainrate(i, 0) + (1.-v)*p_strainrate(i, 1) + v*p_strainrate(i, 2));
+                        F dsigmazz = D0*(v*p_strainrate(i, 0) + v*p_strainrate(i, 1) + (1.-v)*p_strainrate(i, 2));
+                        F dsigmaxy = D0*p_strainrate(i, 3)*(1.-2.*v);
+                        F dsigmaxz = D0*p_strainrate(i, 4)*(1.-2.*v);
+                        F dsigmayz = D0*p_strainrate(i, 5)*(1.-2.*v);
+
+                        // jaumann stress rate
+                        dsigmaxx -= 2.*(p_spinrate(i, 0)*p_sigma(i, 3) + p_spinrate(i, 1)*p_sigma(i, 4));
+                        dsigmayy -= 2.*(-p_spinrate(i, 0)*p_sigma(i, 3) + p_spinrate(i, 2)*p_sigma(i, 5));
+                        dsigmazz += 2.*(p_spinrate(i, 1)*p_sigma(i, 4) + p_spinrate(i, 2)*p_sigma(i, 5));
+                        dsigmaxy += p_sigma(i, 0)*p_spinrate(i, 0) - p_sigma(i, 4)*p_spinrate(i, 2) -
+                            p_spinrate(i, 0)*p_sigma(i, 1) - p_spinrate(i, 1)*p_sigma(i, 5);
+                        dsigmaxz += p_sigma(i, 0)*p_spinrate(i, 1) + p_sigma(i, 3)*p_spinrate(i, 2) -
+                            p_spinrate(i, 0)*p_sigma(i, 5) - p_spinrate(i, 1)*p_sigma(i, 2);
+                        dsigmayz += p_sigma(i, 3)*p_spinrate(i, 1) + p_sigma(i, 1)*p_spinrate(i, 2) +
+                            p_spinrate(i, 0)*p_sigma(i, 4) - p_spinrate(i, 2)*p_sigma(i, 2);
+                        
+                        // update stress state
+                        p_sigma(i, 0) += dt*dsigmaxx;
+                        p_sigma(i, 1) += dt*dsigmayy;
+                        p_sigma(i, 2) += dt*dsigmazz;
+                        p_sigma(i, 3) += dt*dsigmaxy;
+                        p_sigma(i, 4) += dt*dsigmaxz;
+                        p_sigma(i, 5) += dt*dsigmayz;
+
+                        // calculating invariants and deviatoric stress tensor
+                        F I1 = p_sigma(i, 0) + p_sigma(i, 1) + p_sigma(i, 2);
+                        const F s[6] {
+                            p_sigma(i, 0) - I1/3.,
+                            p_sigma(i, 1) - I1/3.,
+                            p_sigma(i, 2) - I1/3.,
+                            p_sigma(i, 3),
+                            p_sigma(i, 4),
+                            p_sigma(i, 5),
+                        };
+                        const F J2 = 0.5*(s[0]*s[0] + s[1]*s[1] + s[2]*s[2] + 2.*(s[3]*s[3] + s[4]*s[4] + s[5]*s[5]));
+                        // tensile correction 1
+                        if (J2 == 0. && I1 > k_c/alpha_phi) {
+                            p_sigma(i, 0) = k_c/alpha_phi/3.;
+                            p_sigma(i, 1) = k_c/alpha_phi/3.;
+                            p_sigma(i, 2) = k_c/alpha_phi/3.;
+                            I1 = k_c/alpha_phi;
+                        }
+
+                        // calculate yield function value
+                        // max used to reduce branching
+                        const F f {alpha_phi*I1 + Kokkos::sqrt(J2) - k_c};
+                        
+                        if (f > 1.e-13) {
+                            const F snorm = 2.*Kokkos::sqrt(J2);
+                            const F shat[6] {
+                                s[0]/snorm,
+                                s[1]/snorm,
+                                s[2]/snorm,
+                                s[3]/snorm,
+                                s[4]/snorm,
+                                s[5]/snorm,
+                            };
+                            const F dfdsig[6] {
+                                alpha_phi + shat[0],
+                                alpha_phi + shat[1],
+                                alpha_phi + shat[2],
+                                shat[3],
+                                shat[4],
+                                shat[5],
+                            };
+                            const F dgdsig[6] {
+                                alpha_psi + shat[0],
+                                alpha_psi + shat[1],
+                                alpha_psi + shat[2],
+                                shat[3],
+                                shat[4],
+                                shat[5],
+                            };
+                            const F dlambda {f/(
+                                dfdsig[0]*D0*((1.-v)*dgdsig[0] + v*dgdsig[1] + v*dgdsig[2]) +
+                                dfdsig[1]*D0*(v*dgdsig[0] + (1.-v)*dgdsig[1] + v*dgdsig[2]) +
+                                dfdsig[2]*D0*(v*dgdsig[0] + v*dgdsig[1] + (1.-v)*dgdsig[2]) +
+                                2.*dfdsig[3]*D0*dgdsig[3]*(1.-2.*v) +
+                                2.*dfdsig[4]*D0*dgdsig[4]*(1.-2.*v) +
+                                2.*dfdsig[5]*D0*dgdsig[5]*(1.-2.*v)
+                            )};
+                            
+                            p_sigma(i, 0) -= dlambda*D0*((1.-v)*dgdsig[0] + v*dgdsig[1] + v*dgdsig[2]);
+                            p_sigma(i, 1) -= dlambda*D0*(v*dgdsig[0] + (1.-v)*dgdsig[1] + v*dgdsig[2]);
+                            p_sigma(i, 2) -= dlambda*D0*(v*dgdsig[0] + v*dgdsig[1] + (1.-v)*dgdsig[2]);
+                            p_sigma(i, 3) -= dlambda*D0*dgdsig[3]*(1.-2.*v);
+                            p_sigma(i, 4) -= dlambda*D0*dgdsig[4]*(1.-2.*v);
+                            p_sigma(i, 5) -= dlambda*D0*dgdsig[5]*(1.-2.*v);
+
+                            I1 = p_sigma(i, 0) + p_sigma(i, 1) + p_sigma(i, 2);
+                            if (I1 > k_c/alpha_phi) {
+                                p_sigma(i, 0) = k_c/alpha_phi/3.;
+                                p_sigma(i, 1) = k_c/alpha_phi/3.;
+                                p_sigma(i, 2) = k_c/alpha_phi/3.;
+                                p_sigma(i, 3) = 0.;
+                                p_sigma(i, 4) = 0.;
+                                p_sigma(i, 5) = 0.;
+                            }
+                        }
                     }
                 };
             }
--- tests/stress_update.cpp
+++ tests/stress_update.cpp
@@ -17,8 +17,7 @@ TEST(stress_update, hookes_law) {
     MPM_system<double, kernels::cubic_bspline<double>, functors::stress_update::hookes_law<double>> 
         myMPM(1, std::array<double, 3>{0., 0., 0.}, std::array<double, 3>{0.99, 1.99, 2.99}, dcell);
 
-    myMPM.f_stress_update.E = 100.;
-    myMPM.f_stress_update.v = 0.25;
+    myMPM.f_stress_update.set_E_v(100., 0.25);
 
     myMPM.p_strainratexx(0) = 1.;
     myMPM.p_strainrateyy(0) = 2.;
@@ -48,8 +47,7 @@ TEST(stress_update, jaumann_rate) {
     MPM_system<double, kernels::cubic_bspline<double>, functors::stress_update::hookes_law<double>> 
         myMPM(1, std::array<double, 3>{0., 0., 0.}, std::array<double, 3>{0.99, 1.99, 2.99}, dcell);
 
-    myMPM.f_stress_update.E = 0.;
-    myMPM.f_stress_update.v = 0.;
+    myMPM.f_stress_update.set_E_v(0., 0.);
 
     myMPM.p_spinratexy(0)= -0.8;
     myMPM.p_spinratexz(0)= -0.9;
@@ -84,41 +82,39 @@ TEST(stress_update, jaumann_rate) {
 
 }
 
-// TEST_CASE("Check DP elasto-plasticity") {
-//     const double dcell = 0.2;
-//     GraMPM::kernel_linear_bspline<double> knl(dcell);
+TEST(stress_update, DP) {
+    
+    const double dcell = 0.2;
 
-//     std::array<double, 3> bf {0., 0., 0.};
-//     GraMPM::MPM_system<double> p(1, bf, knl, std::array<double, 3>{0.}, std::array<double, 3>{0.99, 1.99, 2.99}, dcell);
-//     p.set_stress_update_function(GraMPM::stress_update::drucker_prager_elastoplastic<double>);
-//     const double pi = std::acos(-1.);
-//     p.set_stress_update_param("phi", pi/4.);
-//     p.set_stress_update_param("psi", pi/36.);
-//     p.set_stress_update_param("cohesion", 0.);
+    MPM_system<double, kernels::cubic_bspline<double>, functors::stress_update::drucker_prager_elastoplastic<double>> 
+        myMPM(1, std::array<double, 3>{0., 0., 0.}, std::array<double, 3>{0.99, 1.99, 2.99}, dcell);
+    const double pi = std::acos(-1.);
+    myMPM.f_stress_update.set_DP_params(pi/4., pi/36., 0., 100., 0.25);
 
-//     REQUIRE(p.get_stress_update_param("phi")==pi/4.);
-//     REQUIRE(p.get_stress_update_param("psi")==pi/36.);
-//     REQUIRE(p.get_stress_update_param("cohesion")==0.);
+    ASSERT_DOUBLE_EQ(myMPM.f_stress_update.phi, pi/4.);
+    ASSERT_DOUBLE_EQ(myMPM.f_stress_update.psi, pi/36.);
+    ASSERT_DOUBLE_EQ(myMPM.f_stress_update.cohesion, 0.);
+    ASSERT_DOUBLE_EQ(myMPM.f_stress_update.E, 100.);
+    ASSERT_DOUBLE_EQ(myMPM.f_stress_update.v, 0.25);
 
-//     // check elasto-plastic compression
-//     p.p_strainratexx(0) = -1.;
-//     p.p_strainrateyy(0) = -2.;
-//     p.p_strainratezz(0) = -3.;
-//     p.p_strainratexy(0) = -4.;
-//     p.p_strainratexz(0) = -5.;
-//     p.p_strainrateyz(0) = -6.;
+    // check elasto-plastic compression
+    myMPM.p_strainratexx(0) = -1.;
+    myMPM.p_strainrateyy(0) = -2.;
+    myMPM.p_strainratezz(0) = -3.;
+    myMPM.p_strainratexy(0) = -4.;
+    myMPM.p_strainratexz(0) = -5.;
+    myMPM.p_strainrateyz(0) = -6.;
 
-//     p.set_stress_update_param("E", 100.);
-//     p.set_stress_update_param("v", 0.25);
+    myMPM.h2d();
+    myMPM.p_update_stress(0.1);
+    myMPM.d2h();
 
-//     p.p_update_stress(0.1);
-
-//     REQUIRE(std::round(p.p_sigmaxx(0)*1e8)==-3952509695.);
-//     REQUIRE(std::round(p.p_sigmayy(0)*1e8)==-4496397315.);
-//     REQUIRE(std::round(p.p_sigmazz(0)*1e8)==-5040284935.);
-//     REQUIRE(std::round(p.p_sigmaxy(0)*1e8)==-2175550481.);
-//     REQUIRE(std::round(p.p_sigmaxz(0)*1e8)==-2719438102.);
-//     REQUIRE(std::round(p.p_sigmayz(0)*1e8)==-3263325722.);
+    EXPECT_NEAR(myMPM.p_sigmaxx(0), -39.52509695, 1.e-8);
+    EXPECT_NEAR(myMPM.p_sigmayy(0), -44.96397315, 1.e-8);
+    EXPECT_NEAR(myMPM.p_sigmazz(0), -50.40284935, 1.e-8);
+    EXPECT_NEAR(myMPM.p_sigmaxy(0), -21.75550481, 1.e-8);
+    EXPECT_NEAR(myMPM.p_sigmaxz(0), -27.19438102, 1.e-8);
+    EXPECT_NEAR(myMPM.p_sigmayz(0), -32.63325722, 1.e-8);
 
 //     // check tensile correction
 //     p.p_sigmaxx(0) = 0.;
@@ -141,7 +137,7 @@ TEST(stress_update, jaumann_rate) {
 //     REQUIRE(p.p_sigmaxy(0)==0.);
 //     REQUIRE(p.p_sigmaxz(0)==0.);
 //     REQUIRE(p.p_sigmayz(0)==0.);
-// }
+}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
